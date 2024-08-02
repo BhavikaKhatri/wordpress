f571ec083451cc200eba02b8c6d785213318d21d
bhavika khatri <bhavikakhatri6@gmail.com>
2024-08-02 15:17:57 +0530

    Headless Theme Wordpress

---
+++ wordpress/headless-cms/wp-headless-theme/functions.php
@@ -0,0 +1,53 @@
+<?php
+
+function add_noindex_meta_tag() {
+    if (is_admin()) {
+        echo '<meta name="robots" content="noindex, nofollow" />';
+    }
+}
+add_action('wp_head', 'add_noindex_meta_tag');
+ 
+/**
+ * Nav menus.
+ *
+ * @since v1.0
+ */
+if ( function_exists( 'register_nav_menus' ) ) {
+	register_nav_menus(
+		array(
+			'main-menu'   => 'Main Navigation Menu',
+			'footer-menu' => 'Footer Menu',
+		)
+	);
+}
+
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'post-thumbnails', array( 'post' ) );          // Posts only
+add_theme_support( 'post-thumbnails', array( 'page' ) );  
+
+if( function_exists('acf_add_options_page') ) {
+	
+	$my_options_page = acf_add_options_page(array(
+		'page_title' 	=> 'General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false,
+		'show_in_graphql' => true
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+}
+add_action( 'graphql_should_analyze_queries', '__return_false' );
+
---
+++ wordpress/headless-cms/wp-headless-theme/readme.txt
@@ -0,0 +1,17 @@
+=== Theme Name ===
+Contributors: Bhavika Khatri
+Requires at least: 5.0
+Tested up to: 5.5
+Requires PHP: 7.0
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+
+A WordPress theme to disable the front-end and redirect users to the admin area
+
+== Description ==
+A WordPress theme to disable the front-end and redirect users to the admin area
+
+== Changelog ==
+
+= 1.0 =
+* Release
